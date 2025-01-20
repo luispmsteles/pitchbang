698fd2a16edad8ee66513284265e682339b3865c
luisteles-bw <luis.teles@brightweb.tech>
2025-01-20 01:51:35 +0000

    Setup Sanity cms and created schemas for authors and startups

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
+import "easymde/dist/easymde.min.css";
 
 const workSans = localFont({
     src: [
